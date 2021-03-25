39d8c0293e4739df3ab9027b2797c977f5bccb86
Matimba Mashava <matimbam@discovery.co.za>
2021-03-25 23:38:50 +0200

    First online book store test commit

---
+++ src/main/java/com/matimba/book/shop/assessment/BookShopAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.matimba.book.shop.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShopAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShopAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matimba/book/shop/assessment/BookShopAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matimba.book.shop.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
